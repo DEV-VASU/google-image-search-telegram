3b8d526a37cad4d13c34403e3085e83cc7cdf8e1
DEV-VASU <101116280+DEV-VASU@users.noreply.github.com>
2022-04-15 15:10:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-BOT_NAME = os.environ.get("BOT_NAME", "")
-CSE_KEY = os.environ.get("CSE_KEY", "")
-CSE_CX = os.environ.get("CSE_CX", "")
-API_TOKEN = os.environ.get("API_TOKEN", "")
+BOT_NAME = os.environ.get("BOT_NAME", "ok")
+CSE_KEY = os.environ.get("CSE_KEY", "117549855358257419170")
+CSE_CX = os.environ.get("CSE_CX", "29c252a559c43ec88")
+API_TOKEN = os.environ.get("API_TOKEN", "5104307142:AAFW-V-WmTyb58a4qABgKGhmFDBQsjrDXNo")
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL", "")
 NGINX_SUBPATH = os.environ.get("NGINX_SUBPATH", "")
 BATCH = int(os.environ.get("BATCH", 10))
